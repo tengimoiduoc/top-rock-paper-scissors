d786701583958385be5369e0a00a98b35532812d
Tung <tungdelx2@gmail.com>
2024-09-20 10:25:49 +0700

    create playRound function

--- script.js
+++ script.js
@@ -16,20 +16,58 @@ function computerChoice() {
   }
 }
 
-console.log(computerChoice());
+// console.log(computerChoice());
 
 // Get the human choice, using prompt function
 function humanChoice() {
-  let choice = prompt("Rock, paper or scissor: ");
+  let choice = prompt("Rock, paper or scissors: ");
   return choice.toLocaleLowerCase();
 }
 
-console.log(humanChoice());
+// console.log(humanChoice());
 // play single round
 /*# create playRound function with two 
   parameters (humanChoice, computerChoice), 
-  humanChoice case-sensitive
+  
   #increment the score of human or computer*/
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    // rock < paper
+    console.log("You lose!");
+    computerSCore++;
+    return;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    // rock > scissor
+    console.log("You win, yay!");
+    humanScore++;
+    return;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    // paper < scissor
+    console.log("You lose!:((");
+    computerSCore++;
+    return;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    // paper > rock
+    console.log("You win, yay!");
+    humanScore++;
+    return;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    // scissor < rock
+    console.log("You lose!:((");
+    computerSCore++;
+    return;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    // scissor > paper
+    console.log("You win, :))");
+    humanScore++;
+    return;
+  } else {
+    console.log("Draw!");
+  }
+}
+
+playRound(humanChoice(), computerChoice());
 
 // play the entire game
 /*# create function playGame and move playRound 
